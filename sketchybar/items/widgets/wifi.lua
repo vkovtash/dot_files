586d7fe2937d3fd92ddd4dce3bd1704f55e1c089
Vlad Kovtash <vlad@kovtash.com>
2024-11-04 22:04:31 -0800

    [Sketchybar] Disable network load helper

--- sketchybar/items/widgets/wifi.lua
+++ sketchybar/items/widgets/wifi.lua
@@ -4,8 +4,8 @@ local settings = require("settings")
 
 -- Execute the event provider binary which provides the event "network_update"
 -- for the network interface "en0", which is fired every 2.0 seconds.
-sbar.exec(
-    "killall network_load >/dev/null; $CONFIG_DIR/helpers/event_providers/network_load/bin/network_load en0 network_update 2.0")
+--sbar.exec(
+--    "killall network_load >/dev/null; $CONFIG_DIR/helpers/event_providers/network_load/bin/network_load en0 network_update 2.0")
 
 local popup_width = 250
 
