5b1ded63ef1aa581c37ef79520f2c78bba5ac7af
Vlad Kovtash <vlad@kovtash.com>
2024-11-04 23:02:29 -0800

    [Wezterm] Theme One Dark

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,13 +2,23 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 
-config.color_scheme = 'One Dark (Gogh)'
+config.color_scheme = 'One Dark'
 config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium' })
 config.font_size = 13
 
 config.hide_tab_bar_if_only_one_tab = true
+config.enable_tab_bar = false
 
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = "RESIZE"
+
+config.enable_scroll_bar = true
+
+config.window_padding = {
+  left = 6,
+  right = 12,
+  top = 0,
+  bottom = 0,
+}
 
 return config
