1bca3ca59da5fa12bb95c5cf38e1796850e1e639
Vlad Kovtash <vlad@kovtash.com>
2024-12-28 22:10:00 -0800

    [Sketchybar] Notch

--- sketchybar/items/spaces.lua
+++ sketchybar/items/spaces.lua
@@ -1,55 +1,60 @@
 local colors = require("colors")
 local app_icons = require("helpers.app_icons")
 
-local spaces = {}
+local NOTCH_WIDTH = 200
+local SPACE_COUNT = 8
+local NOTCH_POSITION = SPACE_COUNT / 2
+local EMPTY_SPACE_ICON = "       " .. app_icons["default"]
 
-for i = 1, 10, 1 do
-    local space = sbar.add("space", "space." .. i, {
-        space = i,
+local space_items = {}
+
+-- Notch offset space
+local notch = nil
+
+for i = 1, SPACE_COUNT, 1 do
+    local space_item = sbar.add("item", "space.item." .. i, {
         label = {
-            padding_left = -3,
-            padding_right = 3,
+            string = EMPTY_SPACE_ICON,
+            padding_left = -1,
+            padding_right = 5,
             color = colors.grey,
             highlight_color = colors.white,
             font = "sketchybar-app-font:Regular:16.0",
             y_offset = 0,
+            width = "dynamic",
         },
-        padding_right = 2,
-        padding_left = 2,
+        padding_right = 1,
+        padding_left = 1,
         position = "center",
         background = {
-            color = colors.bar.bg,
-            border_width = 0,
-            height = 25,
-            border_color = colors.black,
+            color = colors.item.bg,
+            border_width = 1,
+            height = 28,
+            border_color = colors.item.border,
         },
+        updates = true,
         popup = { background = { border_width = 2, border_color = colors.black } }
     })
 
-    spaces[i] = space
+    space_items[i] = space_item
 
-    -- Single item bracket for space items to achieve double border on highlight
-    local space_bracket = sbar.add("bracket", { space.name }, {
+    if i == NOTCH_POSITION then
+      notch = sbar.add("item", "space.notch", {
+        width = 0,
         padding_right = 0,
         padding_left = 0,
-        background = {
-            -- color = colors.item.bg,
-            border_color = colors.item.border,
-            height = 28,
-            border_width = 2
-        }
-    })
-
-    -- Padding space
-    sbar.add("space", "space.padding." .. i, {
-        space = i,
-        script = "",
-        width = 2,
         position = "center",
-    })
+        background = {
+            color = colors.item.bg,
+            border_width = 1,
+            height = 28,
+            border_color = colors.item.border,
+        },
+      })
+    end
 
     local space_popup = sbar.add("item", {
-        position = "popup." .. space.name,
+        position = "popup." .. space_item.name,
         padding_left = 5,
         background = {
             drawing = true,
@@ -60,36 +65,24 @@ for i = 1, 10, 1 do
         }
     })
 
+    -- Subscribe to space changes 
+    local space = sbar.add("space", "space." .. i, {
+        space = i,
+        script = "",
+        width = -1,
+        position = "left",
+    })
+
     space:subscribe("space_change", function(env)
         local selected = env.SELECTED == "true"
-        space:set({
-            icon = { highlight = selected, },
-            label = { highlight = selected },
-            background = {
-                border_color = selected and colors.black or colors.item.border,
-                color = colors.item.bg,
-            }
+        space_item:set({
+          icon = { highlight = selected },
+          label = { highlight = selected },
+          background = {
+            border_color = selected and colors.item.border_selected or colors.item.border,
+              color = colors.item.bg,
+          }
         })
-        space_bracket:set({
-            background = {
-                border_color = selected and colors.item.border_selected or colors.item.border,
-                color = colors.item.bg,
-            }
-        })
-    end)
-
-    space:subscribe("mouse.clicked", function(env)
-        if env.BUTTON == "other" then
-            space_popup:set({ background = { image = "space." .. env.SID } })
-            space:set({ popup = { drawing = "toggle" } })
-        else
-            local op = (env.BUTTON == "right") and "--destroy" or "--focus"
-            sbar.exec("yabai -m space " .. op .. " " .. env.SID)
-        end
-    end)
-
-    space:subscribe("mouse.exited", function(_)
-        space:set({ popup = { drawing = false } })
     end)
 end
 
@@ -113,10 +106,22 @@ space_window_observer:subscribe("space_windows_change", function(env)
     end
 
     if (no_app) then
-        icon_line = "       " .. app_icons["default"]
+        icon_line = EMPTY_SPACE_ICON
     end
     sbar.animate("tanh", 10, function()
-        spaces[env.INFO.space]:set({ label = icon_line })
+        space_items[env.INFO.space]:set({ label = icon_line })
     end)
+end)
+
+space_window_observer:subscribe("notch_space_toggle", function()
+  if notch:query().geometry.width > 0 then
+    sbar.animate("tanh", 10, function()
+      notch:set({width = 0})
+    end)
+  else
+    sbar.animate("tanh", 10, function()
+      notch:set({width = NOTCH_WIDTH })
+    end)
+  end
 end)
 
